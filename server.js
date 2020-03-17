6e249918c660b377d1bcbf033152dab6b380d966
Remington Breeze <remington@breeze.software>
2020-03-17 15:16:34 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+// server
+var express = require('express')
+var app = express(); 
+app.use(express.static('public'));
+let port = 5000;
+app.listen(port); 
+console.log("App running on port " + port); 
+
+const rl = require('readline-sync'); 
+const vpns = require('./vpns.json');
+const questions = require('./questions.json');
+
+app.get('/vpns', (req, res) => {
+    res.send({ vpns: vpns }); 
+});
+
+app.get('/questions', (req, res) => {
+    res.send({ questions: questions }); 
+});
