382974d8464f2c6faa5a98d0f485558dcd90d831
Remington Breeze <remington@breeze.software>
2020-03-17 20:25:23 -0700

    Add build directory

---
+++ finder/build/precache-manifest.e307b26e3875e2cbba5079960a902fa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6d66cd3bf7c9e8cff8f8d40844da86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b19e5f5db9ab250dd67f",
+    "url": "/static/css/main.fa8ca4f3.chunk.css"
+  },
+  {
+    "revision": "83b7f34c0d21ad4a421b",
+    "url": "/static/js/2.c7c68428.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.c7c68428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b19e5f5db9ab250dd67f",
+    "url": "/static/js/main.9c715fe7.chunk.js"
+  },
+  {
+    "revision": "8d819729564faf3dba9f",
+    "url": "/static/js/runtime-main.2cb4f374.js"
+  }
+]);
