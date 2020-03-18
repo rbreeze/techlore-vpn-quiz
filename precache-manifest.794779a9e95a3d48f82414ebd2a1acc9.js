d3958291b157ba8888b4caa5ea4fa7e68e98f5d0
Remington Breeze <remington@breeze.software>
2020-03-17 20:41:16 -0700

    Rebuild with different url prefix

---
+++ precache-manifest.794779a9e95a3d48f82414ebd2a1acc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a10cd0ac62961bde569bb7efc036b2a",
+    "url": "/techlore-vpn-quiz/index.html"
+  },
+  {
+    "revision": "403d2140c88dac509f44",
+    "url": "/techlore-vpn-quiz/static/css/main.fa8ca4f3.chunk.css"
+  },
+  {
+    "revision": "83b7f34c0d21ad4a421b",
+    "url": "/techlore-vpn-quiz/static/js/2.c7c68428.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/techlore-vpn-quiz/static/js/2.c7c68428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "403d2140c88dac509f44",
+    "url": "/techlore-vpn-quiz/static/js/main.38a0957e.chunk.js"
+  },
+  {
+    "revision": "3ecb35ce6168968da92d",
+    "url": "/techlore-vpn-quiz/static/js/runtime-main.bdf12397.js"
+  }
+]);
